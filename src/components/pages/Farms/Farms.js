adc2eb5eced3014d9a1f6dabae5bf044fb3b91aa
unknown <babatundeyusuffolorunso@gmail.com>
2023-05-21 14:52:24 +0100

    adding farm page

--- src/components/pages/Farms/Farms.js
+++ src/components/pages/Farms/Farms.js
@@ -1,6 +1,7 @@
-import { makeStyles } from "@material-ui/core";
+import { makeStyles,Card } from "@material-ui/core";
 import { connect } from "react-redux";
 import { useEffect, useMemo, useState } from "react";
+import { Redirect } from 'react-router-dom';
 import TabPage from "../../TabPage";
 import Farm from "./Farm";
 import {
@@ -22,6 +23,7 @@ const useStyles = makeStyles((theme) => ({
     fontSize: 36,
     fontWeight: 600,
   },
+  // . "#370937"  '#a3c1ff'
   subTitle: {
     textAlign: "center",
     color: theme.palette.textColors.subheadingWhite,
@@ -32,9 +34,77 @@ const useStyles = makeStyles((theme) => ({
       fontWeight: 600,
     },
   },
+  // !folly
+  card: {
+    width: "96%",
+    maxWidth: 500,
+    borderRadius: 30,
+    boxShadow: `rgb(0 0 0 / 1%) 0px 0px 1px, rgb(0 0 0 / 4%) 0px 4px 8px, rgb(0 0 0 / 4%) 0px 16px 24px, rgb(0 0 0 / 1%) 0px 24px 32px`,
+    backgroundColor: theme.palette.primary.bgCard,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 15,
+    paddingBottom: 25,
+    marginLeft:"auto",
+    marginRight:"auto",
+    [theme.breakpoints.down("sm")]: {
+      paddingLeft: 7,
+      paddingRight: 7,
+      width: "96%",
+    },
+  },
+  heading: {
+    // color: theme.palette.textColors.headingwhite,
+    color: '#a3c1ff',
+    letterSpacing: -0.7,
+    fontSize: 20,
+    fontWeight: 600,
+    textAlign:'center',
+  },
+  cardContents: {
+    display: "flex",
+    justifyContent:'center',
+    flexDirection: "column",
+    // alignItems: "center",
+  },
+  cardHeading: {
+    paddingTop: 5,
+    width: "95%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: theme.palette.primary.iconColor,
+
+    [theme.breakpoints.down("sm")]: {
+      paddingTop: 0,
+    },
+  },
+  cardFeature: {
+    marginTop: 10,
+    marginBottom: 10,
+    width: "95%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    // justifyContent: "space-between",
+    alignItems: "center",
+    padding:'20 40px'
+  },
+  cardTitle: {
+    fontSize: 16,
+
+    color: theme.palette.textColors.subheading,
+
+    [theme.breakpoints.down("sm")]: {
+      fontSize: 16,
+    },
+  },
+  // !
+  
 }));
 
 const Farms = (props) => {
+  // 
   const {
     dex: { transaction },
     account: { currentChain },
@@ -97,6 +167,7 @@ const Farms = (props) => {
     }
   };
 
+
   // useEffect(() => {
   //   console.log("farm price token price ", { farmTokenPriceUsd });
   // }, [farmTokenPriceData]);
@@ -132,14 +203,29 @@ const Farms = (props) => {
       <div>
         <TabPage data={2} />
       </div>
-      <div className="mt-5 mb-2 container row">
+      <Card elevation={20} className={classes.card}>
+      <div className={classes.cardContents}>
+          <div className={classes.cardHeading}>
+            <h6 className={classes.title}>Farm</h6>     
+          </div>
+          <div className={classes.cardFeature}>     
+      <h1 className={classes.heading}>Coming Soon</h1>
+      <p className={classes.heading}>Farming will be available soon on</p>
+          </div>
+
+        
+
+          </div>
+
+      </Card>
+      {/* <div className="mt-5 mb-2 container row">
         <div className={classes.subTitle}>
           {farmPools?.length === 0
             ? "Farming will be available soon on " + currentNetwork + " network"
             : "Stake LP tokens to Earn Rewards"}
         </div>
-      </div>
-      <div
+      </div> */}
+      {/* <div
         className="container row flex-row justify-content-center align-items-center mb-5"
         align="center"
       >
@@ -163,7 +249,7 @@ const Farms = (props) => {
             handleClose={handleDialogClose}
           />
         </div>
-      </div>
+      </div> */}
     </>
   );
 };
